9b139b43687e731884562e3392935f217d983d7b
Thomas Berk <tberk@gmx.at>
2018-11-08 21:08:02 +0100

    some translations added to dictionary for tr()

--- Script/translation.py
+++ Script/translation.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 
 
 dictionary = {
+	"homeNOW": "homeNOW",
+	"Dashboard": "Dashboard",
+	"HomeIndicator": "HomeIndicator",
+	"Menu": "Menü","
 	"Rooms": "Räume",
 	"Functions": "Gewerke",
 	"Favorites": "Favoriten",
 	"Device": "Gerät",
 	"Devices": "Geräte",
 	"Channel": "Kanal",
-	"Channels": "Kanäle"
+	"Channels": "Kanäle",
+	
+	"Settings": "Einstellungen",
+	"Edit Page": "Seite Editieren","
+	"Username": "Benutzername",
+	"Password": "Passwort",
+	"IP Address": "IP-Adresse",
+	"or": "oder",
+	"and": "und",
+	"Server address": "Server-Adresse",
+	"": ""
 }
 
 def tr(word, language=None):
